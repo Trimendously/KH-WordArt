10dcd92c3ad1eb6af4b8400ab68749508f82f7ba
Noah Trim <55414467+Trimendously@users.noreply.github.com>
2021-03-23 02:41:55 -0400

    New GUI
    Need to better the look of the UI

--- keyblade.py
+++ keyblade.py
@@ -1,30 +1,46 @@
 from PIL import Image,ImageFont,ImageDraw
 from random import randint
 
+# GUI Tool
+import tkinter as tk
+
+# To get all png files in dir
+import glob, os
+
 def random_color():
     r=randint(50,255)
     g=randint(50,255)
     b=randint(50,255)
     return r,g,b
-def choice():
-    while True:
-        choices = ['DARKNESS','LIGHT','FRIENDS','HEART'] #Most used words in KH
-        print("Your options are : ", choices)
-        word =input("Enter word:  ") #Word to make art
-        word = word.upper()
-        if word in choices:
-            break
+def choice(png):
+    choicePage = tk.Tk()
+    width = choicePage.winfo_screenwidth()
+    height = choicePage.winfo_screenheight()
+    choicePage.geometry(str(width)+ "x" + str(height)) #Sets resolution
+    label1 = tk.Label(text="Enter the word you would like to use?")
+    label.grid(row = 0,column = 0)
+    entry1 = tk.Entry(choicePage)
+    entry1.grid(row = 1,column = 0)
 
-    return word
-def wordart():
-    word = choice()
+    # Bad implementation (need to fix)
+    def confirm():
+        wordart(png,entry1.get().upper())
+    confirm_button = tk.Button(choicePage,text='Confirm',command = confirm)
+    confirm_button.grid(row = 2,column = 0)
 
+    choicePage.mainloop()
+def wordart(png,word):
+    print(word)
     text = word * 100 #Number of times to repeat word
 
-    # Establish a black screen for base
-    base =Image.new('RGBA',(900,900),'black')
+    img = Image.open(png, "r").convert("RGB")
+    img_h,img_w = img.size
+    pixel = img.load()
 
-    font=ImageFont.truetype("arial.ttf",10)
+    # Establish a black screen for base
+    base =Image.new('RGBA',(img_h,img_w),'black')
+    text_size = int(img_w / 50) #Making text size according to dimension
+    font=ImageFont.truetype("arial.ttf",text_size)
     width, height= font.getsize(text)
     draw=ImageDraw.Draw(base)
 
@@ -38,31 +54,38 @@ def wordart():
         r, g, b = random_color()
         start= randint(0,len(word)-1) # Staggers the text
         draw.text((0,height_counter),word[start:len(word)]+text, font=font, fill=(r,g,b))
-        height_counter = height_counter + height
-
-    art = Image.new('RGBA',(900,900),'black')
-    font1 = ImageFont.truetype("arial.ttf",200)
-    width, height = font1.getsize(silhoutte)
-    draw1=ImageDraw.Draw(art)
-    draw1.text(((art.size[0]-width)/2,(art.size[1]-height)/2),silhoutte, font=font1, fill='white')
+        height_counter += height
 
     hor = []
     vert = []
     # Formats text into white space of image
-    for i in range(art.size[0]):
-        for j in range(art.size[1]):
-            pixel=art.getpixel((i,j))
-            if (pixel == (255,255,255,255)):
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            if (pixel[i,j] != (255,255,255)):
                 hor.append(i)
                 vert.append(j)
-                #art.putpixel((i,j),base.getpixel((i,j)))
 
     # Gets rid of outline of image
-    final_art =Image.new('RGBA',(900,900),'black')
+    final_art =Image.new('RGBA',(img_h,img_w),'black')
     for i,j in zip(hor,vert):
             final_art.putpixel((i,j),base.getpixel((i,j)))
 
     final_art.show()
     final_art.save("wordart.png")
 
-wordart()
+window = tk.Tk()
+width = window.winfo_screenwidth()
+height = window.winfo_screenheight()
+window.geometry(str(width)+ "x" + str(height)) #Sets resolution
+window.columnconfigure(0,weight=1)
+label = tk.Label(text="Select an image that you would like to use: ")
+label.grid(row=0,column=0)
+
+#Gets all png files in the dir and makes them into buttons
+for i,file in enumerate(glob.glob(os.getcwd()+ "/**/*.png",recursive=True),start = 1):
+    png = os.path.basename(file) #Gets file name from path
+    b = tk.Button(window,text=png)
+    b.grid(row=i, column=0)
+    b.config(command = lambda b= b: choice(b['text']))
+
+window.mainloop()
